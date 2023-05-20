73a03b80e735184e8dd9d1f476e84c8f7df7b81a
Triệu Lê Hoàng <hoangtl@timebird.org>
2023-05-20 11:15:04 +0700

    first commit

--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ const header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-const btnMenuMobile = document.querySelector(".button__mobile");
+const btnMenuMobile = document.querySelector(".header-button__mobile");
 btnMenuMobile.addEventListener('click', appearMenuMobile)
 
 var isAppearMenuMobile = false
@@ -11,15 +11,18 @@ var isAppearMenuMobile = false
 function appearMenuMobile() {
   isAppearMenuMobile = !isAppearMenuMobile
   var menuMobile = document.getElementById('navbar__menu')
-  var iconMenuMobile = document.getElementById('btn__mobile')
+  var iconMenuMobile = document.getElementById('button-mobile')
+  var btnHeaderMobile = document.getElementById('button-sign-up__mobile')
   if (isAppearMenuMobile) {
     menuMobile.classList.add('menu-mobile')
     iconMenuMobile.classList.remove('fa-bars-staggered')
     iconMenuMobile.classList.add('fa-arrow-right')
+    btnHeaderMobile.classList.add('header-button-sign-up__mobile')
   }
   else {
     menuMobile.classList.remove('menu-mobile')
     iconMenuMobile.classList.remove('fa-arrow-right')
     iconMenuMobile.classList.add('fa-bars-staggered')
+    btnHeaderMobile.classList.remove('header-button-sign-up__mobile')
   }
 }
